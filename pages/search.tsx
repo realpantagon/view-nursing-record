e28cfc774c4bf3b8970319b75d9806c286b30e6b
Relpantagon <pichayoothunchainao@gmail.com>
2023-12-14 23:26:39 +0700

    card  template

--- pages/search.tsx
+++ pages/search.tsx
@@ -17,10 +17,10 @@ export default function search() {
   return (
     <div className={styles.container}>
       <Navbar />
-        <h1 className={styles.font}>ค้นหาผู้ป่วย</h1>
+      <h1 className={styles.font}>ค้นหาผู้ป่วย</h1>
       <div className={styles.barcontainer}>
         <div className={styles.butgroup}>
-          <div className={`p-inputgroup flex-1 sm:max-w-full md:max-w-md` } >
+          <div className={`p-inputgroup flex-1 sm:max-w-full md:max-w-full`}>
             <Dropdown
               value={selectedCity}
               onChange={(e) => setSelectedCity(e.value)}
@@ -32,9 +32,24 @@ export default function search() {
             <InputText placeholder="Keyword" className={styles.input} />
             <Button label="ค้นหา" className="" />
           </div>
-        <div className={styles.contentcontainer}>
-        
-        </div>
+          <div className={styles.contentcontainer}>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 place-items-center my-auto mx-auto">
+              <div className="col-span-1 bg-white py-8 px-16  rounded-md shadow-md">
+                <div>
+                  ชื่อ: <b>พิชยุทธ หันชัยเนาว์</b>
+                </div>
+                <div>
+                  Bed number: <b>34</b>
+                </div>
+                <div>
+                  Admit Number:<b>099</b>
+                </div>
+                <div>
+                  เวลาบันทึกล่าสุด <b>10:34 12/12/2023</b>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
